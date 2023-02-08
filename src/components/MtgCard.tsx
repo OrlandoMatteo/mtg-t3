cc494779e93d385bdb51e575b74cf92bfd72b5a2
Matteo Orlando <matteo.orlando.1993@gmail.com>
2023-02-08 10:01:34 +0100

    update mtgcard

--- src/components/MtgCard.tsx
+++ src/components/MtgCard.tsx
@@ -1,57 +1,53 @@
 import symbols from "../utils/symbology.json";
 import { api } from "../utils/api";
+import { map } from "@trpc/server/observable";
 function getSymbolsSVG(manaCost: string): Array<string> {
 	let symbolURLs: Array<string> = [];
-	const extractedArray:Array<string> = manaCost?.match(/\{[^}]+\}/g)||[''];
+	const extractedArray: Array<string> = manaCost?.match(/\{[^}]+\}/g) || [''];
 	extractedArray.forEach((symbol) => {
-	let symbolinfo = symbols.data.find((x) => x.symbol == symbol) || {
-		svg_uri: "",
-	};
-	symbolURLs.push(symbolinfo.svg_uri);
-});
+		let symbolinfo = symbols.data.find((x) => x.symbol == symbol) || {
+			svg_uri: "",
+		};
+		symbolURLs.push(symbolinfo.svg_uri);
+	});
 	return symbolURLs
 }
-export default function MtgCard(): JSX.Element {
-	const card = api.card.randomCard.useQuery()
-	//	const imgResponse = api.card.imageSrc.useQuery({ 'src': card.data?.identifiers?.scryfallOracleId! })
-	//	const url = "https://api.scryfall.com/cards/" + imgResponse.data?.identifiers.scryfallId + "?format=json"
-	//	const res = await fetch(url)
-	//	const data = await res.json()
-	//	console.log(imgResponse.data)
-	//	const uri = data.info.image_uris.art_crop
-	const manaCost = card.data?.manaCost
-	const extractedArray = manaCost?.match(/\{[^}]+\}/g);
-	console.log("manaCost")
-	console.log(card.data?.name)
-	//let symbolURLs: Array<string> = [];
-	//Array.from(extractedArray).forEach((symbol) => {
-	//	let symbolinfo = symbols.data.find((x) => x.symbol == symbol) || {
-	//		svg_uri: "",
-	//	};
-	//	symbolURLs.push(symbolinfo.svg_uri);
-	//});
-	//const cardColors: Array<string> = card.data?.colors || ["B"]
-	//const colors: { [key: string]: string } = {
-	//	U: "#c1d7e9",
-	//	W: "#f8f6d8",
-	//	R: "#e49977",
-	//	B: "#cac5c0",
-	//	G: "#a3c095",
-	//};
-	//let monoColor = "slate";
-	//let startGradient = "slate";
-	//let endGradient = "slate";
-	//let isSingle = true;
-	//if (cardColors.length == 1) {
-	//	monoColor = colors[cardColors[0]] || "slate";
-	//} else if (cardColors.length == 2) {
-	//	isSingle = false;
-	//	startGradient = colors[cardColors[0]];
-	//	endGradient = colors[cardColors[1]];
-	//}
-	return (<div><div>{card.data?.name}</div>
-		{getSymbolsSVG(card.data?.manaCost).map((uri)=><img class="w-6" src={uri}/>)}</div>);
 
+function getBackground(cardColors: Array<string> = [""]): Object {
+	const colors: { [key: string]: string } = {
+		U: "blue-200",
+		W: "yellow-50",
+		R: "red-300",
+		B: "slate-300",
+		G: "green-300",
+	};
+	let output = { startGradient: 'slate-300', endGradient: 'slate-400' }
+	if (cardColors.length == 1) {
+		output.startGradient = colors[cardColors[0]] || "slate-400";
+		output.endGradient = colors[cardColors[0]] || "slate-300";
+	} else if (cardColors.length == 2) {
+		output.startGradient = colors[cardColors[0]] || "slate-300";
+		output.endGradient = colors[cardColors[1]] || "slate-400";
+	}
+
+	return output
+}
+export default function MtgCard() {
+	const card = api.card.randomCard.useQuery()
+	const colors = getBackground(card.data?.colors) ?? { startGradient: 'slate-500', endGradient: 'slate-400' }
+	return (
+		<div className={" bg-gradient-to-r from-" +
+			colors.startGradient + " to-" + colors.endGradient + " w-11/12 md:w-1/3 m-auto py-1 border-solid border-slate-700 border-4 rounded-lg h-[80vh] flex flex-col"}  >
+			<div className="flex flex-row w-10/12 m-auto rounded-lg my-4 shadow-inner shadow-2xl p-2">
+				<h1 className="text-black text-xl font-bold basis-5/6">
+					{card.data?.name}
+				</h1>
+				{getSymbolsSVG(card.data?.manaCost).map((uri) => <img className="w-6" src={uri} />)}
+			</div>
+		</div>
+
+
+	);
 
 
 }
--- src/server/api/routers/card.ts
+++ src/server/api/routers/card.ts
@@ -7,7 +7,7 @@ export const cardRouter = createTRPCRouter({
     const randomehdRank: number = Math.floor(Math.random() * 24425)
     return ctx.prisma.cards.findFirst(
       { where: { edhrecRank: randomehdRank },
-        select:{manaCost:true,name:true}})
+        select:{manaCost:true,name:true,colors:true}})
   }),
   imageSrc: publicProcedure
     .input(z.object({ src: z.string() }))
